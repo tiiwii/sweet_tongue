aec9ae7fdbbd93369b2733fe6576cfaa970c517a
tiiwii <miccvys@gmail.com>
2021-09-21 01:29:40 +0300

    add loader

--- js/script.js
+++ js/script.js
@@ -83,3 +83,15 @@ window.onsroll = () => {
     });
 }
 
+
+/* ---------- loader ----------- */
+function loader(){
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut() {
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut;
+
